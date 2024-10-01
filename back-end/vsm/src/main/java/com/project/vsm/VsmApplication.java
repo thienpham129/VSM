98de3a79d82fc199ebad03113466adc3e3ee60a2
nhatnd0909 <nguyendinhnhat0909@gmail.com>
2024-10-01 20:52:39 +0700

    first commit

---
+++ back-end/vsm/src/main/java/com/project/vsm/VsmApplication.java
@@ -0,0 +1,13 @@
+package com.project.vsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsmApplication.class, args);
+	}
+
+}
