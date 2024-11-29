8ae3fe20d791f89bf8c34013441dd4aefb64d51e
DuyCoder <duynv.it.052@gmail.com>
2024-11-29 14:49:04 +0700

    update filter route

--- back-end/vsm/src/main/java/com/project/vsm/repository/ScheduleRepository.java
+++ back-end/vsm/src/main/java/com/project/vsm/repository/ScheduleRepository.java
@@ -72,4 +72,8 @@ public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> 
 
 	Optional<ScheduleEntity> findByAccount_IdAndStartTime(Long accountId, LocalDateTime startTime);
 
+	@Query("SELECT s FROM ScheduleEntity s WHERE s.route.id = :routeId AND DATE(s.startTime) = :startTime")
+	List<ScheduleEntity> findByRouteIdAndStartTime(@Param("routeId") long routeId, @Param("startTime") LocalDate startTime);
+
+
 }
