bbdd46ac764cc4061ed3f37cc3c2cb106b912431
XQuang1607 <quangnxde160580@fpt.edu.vn>
2025-01-16 20:58:06 +0700

    update full code #2

--- back-end/vsm/src/main/java/com/project/vsm/controller/PaymentController.java
+++ back-end/vsm/src/main/java/com/project/vsm/controller/PaymentController.java
@@ -1,6 +1,5 @@
 package com.project.vsm.controller;
 
-
 import com.project.vsm.dto.response.ResponseObject;
 import com.project.vsm.dto.response.TicketGoogleSheetResponse;
 import com.project.vsm.dto.response.VNPayResponse;
@@ -22,59 +21,60 @@ import java.util.Enumeration;
 @RequestMapping("/api/v1/payment")
 @RequiredArgsConstructor
 public class PaymentController {
-    private final PaymentService paymentService;
-    private final TicketRepository ticketRepository;
-    private final ScheduleRepository scheduleRepository;
+	private final PaymentService paymentService;
+	private final TicketRepository ticketRepository;
+	private final ScheduleRepository scheduleRepository;
 
-    @GetMapping("/pay/{ticketId}")
-    public ResponseObject<VNPayResponse> payForTicket(@PathVariable String ticketId) {
-       try {
-           return ResponseObject.<VNPayResponse>builder()
-                   .code(200)
-                   .message("General QR successfully")
-                   .data(paymentService.generatePaymentUrl(ticketId))
-                   .build();
-       }catch (RuntimeException e){
-           return ResponseObject.<VNPayResponse>builder()
-                   .code(400)
-                   .message("General QR fail!")
-                   .build();
-       }
-    }
-    @GetMapping("/vn-pay")
-    public ResponseObject<VNPayResponse> pay(@RequestParam String ticketId, HttpServletRequest request){
-        VNPayResponse response = paymentService.createVnPayPayment(ticketId,request);
-        return new ResponseObject<>(HttpStatus.OK.value(), "Success", response);
-    }
+	@GetMapping("/pay/{ticketId}")
+	public ResponseObject<VNPayResponse> payForTicket(@PathVariable String ticketId) {
+		try {
+			return ResponseObject.<VNPayResponse>builder().code(200).message("General QR successfully")
+					.data(paymentService.generatePaymentUrl(ticketId)).build();
+		} catch (RuntimeException e) {
+			return ResponseObject.<VNPayResponse>builder().code(400).message("General QR fail!").build();
+		}
+	}
 
-    @GetMapping("/vn-pay-callback")
-    public void payCallbackHandler(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String status = request.getParameter("vnp_ResponseCode");
-        String ticketId = request.getParameter("vnp_TxnRef");
+	@GetMapping("/vn-pay")
+	public ResponseObject<VNPayResponse> pay(@RequestParam String ticketId, HttpServletRequest request) {
+		VNPayResponse response = paymentService.createVnPayPayment(ticketId, request);
+		return new ResponseObject<>(HttpStatus.OK.value(), "Success", response);
+	}
 
-        if ("00".equals(status)) {
-            // Tìm và cập nhật trạng thái vé
-            TicketEntity ticket = ticketRepository.findById(ticketId)
-                    .orElseThrow(() -> new RuntimeException("Ticket not found"));
-            ScheduleEntity scheduleEntity = ticket.getScheduleEntity();
-            if (scheduleEntity != null) {
-                int emptySeat = scheduleEntity.getEmptySeat();
-                int seatsBooked = ticket.getSelectedSeat().split(",").length;
-                if (emptySeat >= seatsBooked) {
-                    scheduleEntity.setEmptySeat(emptySeat - seatsBooked);
-                    scheduleRepository.save(scheduleEntity);
-                    ticket.setStatus("Đã thanh toán");
-                    ticket.setPaid(true);
-                    ticketRepository.save(ticket);
-                }
-            }
+	@GetMapping("/vn-pay-callback")
+	public void payCallbackHandler(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String status = request.getParameter("vnp_ResponseCode");
+		String ticketId = request.getParameter("vnp_TxnRef");
 
-            // Chuyển hướng đến trang paymentSuccess
-            response.sendRedirect("http://vsmcar.shop/paymentSuccess?ticketId=" + ticketId + "&status=success");
-        } else {
-            // Nếu thanh toán thất bại, chuyển hướng đến trang thất bại
-            response.sendRedirect("http://vsmcar.shop/paymentFailure?ticketId=" + ticketId + "&status=failure");
-        }
-    }
+		if ("00".equals(status)) {
+			// Tìm và cập nhật trạng thái vé
+			TicketEntity ticket = ticketRepository.findById(ticketId)
+					.orElseThrow(() -> new RuntimeException("Ticket not found"));
+			ScheduleEntity scheduleEntity = ticket.getScheduleEntity();
+			if (scheduleEntity != null) {
+				int emptySeat = scheduleEntity.getEmptySeat();
+				int seatsBooked = ticket.getSelectedSeat().split(",").length;
+				if (emptySeat >= seatsBooked) {
+					scheduleEntity.setEmptySeat(emptySeat - seatsBooked);
+					scheduleRepository.save(scheduleEntity);
+					ticket.setStatus("Đã thanh toán");
+					ticket.setPaid(true);
+					ticketRepository.save(ticket);
+				}
+			}
+
+			// URL thành công, kèm theo tham số ticketId và status
+			String successUrl = "http://localhost:3000/paymentSuccess?ticketId=" + ticketId + "&status=success";
+
+			// Redirect đến trang thành công
+			response.sendRedirect(successUrl);
+		} else {
+			// Trường hợp thanh toán thất bại
+			String failureUrl = "http://localhost:3000/paymentFailure?ticketId=" + ticketId + "&status=failure";
+
+			// Redirect đến trang thất bại
+			response.sendRedirect(failureUrl);
+		}
+	}
 
 }
